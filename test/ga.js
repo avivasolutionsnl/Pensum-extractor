2e85d45282892a34e919d9d2564712cd1afb563c
Joost Meijles <joost@meijles.com>
2020-02-21 10:31:27 +0100

    Small test refactoring

--- test/ga.js
+++ test/ga.js
@@ -3,14 +3,14 @@ import assert from 'assert';
 
 describe('GA', function () {
     describe('#convertToPageVisit()', function () {
-        const element = {
-            dimensions: ['/', '(entrance)'],
-            metrics: [{
-                values: ['14', '87', '14', '5', '4', '1', '0']
-            }]
-        };
-
         it('first element should convert to correct user and page visit', function () {
+            const element = {
+                dimensions: ['/', '(entrance)'],
+                metrics: [{
+                    values: ['14', '87', '14', '5', '4', '1', '0']
+                }]
+            };
+
             const visit = convertToPageVisit(element);
             assert.strictEqual(visit.page, '/');
             assert.strictEqual(visit.previousPage, 'entrance');
@@ -18,14 +18,14 @@ describe('GA', function () {
             assert.strictEqual(visit.events.length, 2);
         });
 
-        const element2 = {
-            dimensions: ['/http://localhost/search?location=searchtext=test', '/'],
-            metrics: [{
-                values: ['2', '24', '0', '1', '0', '0', '0']
-            }]
-        };
-
         it('second element should convert to correct visit', function () {
+            const element2 = {
+                dimensions: ['/http://localhost/search?location=searchtext=test', '/'],
+                metrics: [{
+                    values: ['2', '24', '0', '1', '0', '0', '0']
+                }]
+            };
+
             const visit = convertToPageVisit(element2);
             assert.strictEqual(visit.page, '/search');
             assert.strictEqual(visit.previousPage, '/');
--- test/gaCustom.js
+++ test/gaCustom.js
@@ -3,14 +3,14 @@ import assert from 'assert';
 
 describe('GA-custom', function () {
     describe('#convertToUserVisit()', function () {
-        const element = {
-            dimensions: ['1', '1553847449714', '1', '/'],
-            metrics: [{
-                values: ['1', '10', '1', '0', '1', '0', '0']
-            }]
-        };
-
         it('first element should convert to correct user and page visit', function () {
+            const element = {
+                dimensions: ['1', '1553847449714', '1', '/'],
+                metrics: [{
+                    values: ['1', '10', '1', '0', '1', '0', '0']
+                }]
+            };
+
             const visit = convertToUserVisit(element);
             assert.strictEqual(visit.identifier, '1_1');
             assert.strictEqual(visit.page, '/');
@@ -19,14 +19,14 @@ describe('GA-custom', function () {
             assert.strictEqual(visit.events[0].name, 'adds-to-cart');
         });
 
-        const element2 = {
-            dimensions: ['2', '1553847449714', '1', '/http://localhost/search?location=searchtext=test'],
-            metrics: [{
-                values: ['1', '3', '1', '0', '1', '1', '0']
-            }]
-        };
-
         it('second element should convert to correct visit', function () {
+            const element2 = {
+                dimensions: ['2', '1553847449714', '1', '/http://localhost/search?location=searchtext=test'],
+                metrics: [{
+                    values: ['1', '3', '1', '0', '1', '1', '0']
+                }]
+            };
+
             const visit = convertToUserVisit(element2);
             assert.strictEqual(visit.identifier, '2_1');
             assert.strictEqual(visit.page, '/search');
@@ -36,14 +36,14 @@ describe('GA-custom', function () {
             assert.strictEqual(visit.events[1].name, 'removes-from-cart');
         });
 
-        const element3 = {
-            dimensions: ['2', '1553847449714', '2', '/drinks/page'],
-            metrics: [{
-                values: ['1', '7', '0', '1', '0', '0', '0']
-            }]
-        };
-
         it('third element should convert to correct visit', function () {
+            const element3 = {
+                dimensions: ['2', '1553847449714', '2', '/drinks/page'],
+                metrics: [{
+                    values: ['1', '7', '0', '1', '0', '0', '0']
+                }]
+            };
+
             const visit = convertToUserVisit(element3);
             assert.strictEqual(visit.identifier, '2_2');
             assert.strictEqual(visit.page, '/drinks/page');
@@ -51,14 +51,14 @@ describe('GA-custom', function () {
             assert.strictEqual(visit.events.length, 0);
         });
 
-        const element4 = {
-            dimensions: ['3', '1553847449714', '1', '/drinks'],
-            metrics: [{
-                values: ['1', '1', '1', '0', '0', '0', '1']
-            }]
-        };
-
         it('fourth element should convert to correct visit', function () {
+            const element4 = {
+                dimensions: ['3', '1553847449714', '1', '/drinks'],
+                metrics: [{
+                    values: ['1', '1', '1', '0', '0', '0', '1']
+                }]
+            };
+
             const visit = convertToUserVisit(element4);
             assert.strictEqual(visit.identifier, '3_1');
             assert.strictEqual(visit.page, '/drinks');
